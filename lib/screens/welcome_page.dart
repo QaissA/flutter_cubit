97400718562f882f1370a3919c944e612ea42d3f
qaiss-1996 <qaissabdelhamid@gmail.com>
2022-08-21 00:02:50 +0200

    First commit'

---
+++ lib/screens/welcome_page.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/animation/animation_controller.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/ticker_provider.dart';
+import 'package:flutter_cubit/misc/colors.dart';
+import 'package:flutter_cubit/widget/app_large_text.dart';
+import 'package:flutter_cubit/widget/app_text.dart';
+import 'package:flutter_cubit/widget/responsive_button.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List images = [
+      "welcome-one.png",
+      "welcome-two.png",
+      "welcome-three.png",
+    ];
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('img/' + images[index]),
+                    fit: BoxFit.cover),
+              ),
+              child: Container(
+                margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppLargeText(text: 'Trips'),
+                        AppText(
+                          text: 'Mountain',
+                          size: 30,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: 250,
+                          child: AppText(
+                            text:
+                                'Mountain hikes give you an incredible sense of freedem along with endurance tests',
+                            color: AppColors.textColor2,
+                            size: 14,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        ResponsiveButton(
+                          width: 120,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: List.generate(3, (indexDots) {
+                        return Container(
+                          margin: EdgeInsets.all(5),
+                          width: 8,
+                          height: index == indexDots ? 25 : 8,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: index == indexDots
+                                  ? AppColors.mainColor
+                                  : AppColors.mainColor.withOpacity(0.3)),
+                        );
+                      }),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
